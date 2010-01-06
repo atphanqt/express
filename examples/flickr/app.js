f1a95fcaa82e44b1eda5b259cc3f2df3b2b6d662
ciaranj <ciaranj@gmail.com>
2010-01-06 21:15:37 +0000

    The flickr integration begins....

--- examples/flickr/app.js
+++ examples/flickr/app.js
@@ -1,6 +1,5 @@
 var sys= require('sys');
 require.paths.unshift('lib')
-var flickr= require('support/flickr/lib/flickr')
 require('express')
 require('express/plugins')
 
@@ -13,47 +12,45 @@ configure(function(){
   enable('cache statics')
 })
 
-/*
+var FlickrAPI= require('support/flickr/lib/flickr').FlickrAPI;
+
 function do_index(arguments) {
     this.render('index.haml.html', {
         locals: arguments
       });
-}*/
-/*
+}
+
 function fail(err) {
     sys.puts(err)
     
     throw new Error(err.code + " -  " + err.message);
 };
-    */
+    
 get('/', function(){
-     sys.puts('fooo');
-     this.render('index.haml.html', {
-         locals: {
-           user_name : '',
-           api_key : ''
-         }
-       });     
+     do_index.apply(this,[{
+        user_name : '',
+        api_key : '',
+        photos : []
+      }])
 })
 
-/*post('/', function() {
+post('/', function() {
     var user_name= this.param('user_name');
     var api_key= this.param('api_key');
-//    sys.puts('fooo');
-
-    flickr.people.findByUsername(user_name,api_key).addErrback(fail).addCallback(function(user){
-  //      sys.puts('found user' +  user.id)
-        flickr.photosets.getList(user.id, api_key).addErrback(fail).addCallback(function(photosets){
-            flickr.photosets.getPhotos(photosets.photoset[0].id, api_key, ["url_sq", "url_m"]).addErrback(fail).addCallback(function(photoset){
-    //            sys.puts("FOO: " + sys.inspect(photoset));
-                do_index.apply(this, [{
+    var flickr= new FlickrAPI(api_key);
+    var request= this;
+    flickr.people.findByUsername(user_name).addErrback(fail).addCallback(function(user){
+        flickr.photosets.getList(user.id).addErrback(fail).addCallback(function(photosets){
+            flickr.photosets.getPhotos(photosets.photoset[0].id, ["url_sq", "url_m"]).addErrback(fail).addCallback(function(photoset){
+                do_index.apply(request, [{
                   user_name : user_name == null ? '' : user_name ,
-                  api_key : api_key == null ? '' : api_key
+                  api_key : api_key == null ? '' : api_key,
+                  photos : photoset.photo === undefined ? [] : photoset.photo
                 }])
             });
         });
     });    
-})*/
+})
 
 get('/public/*', function(file){
   this.sendfile(dirname(__filename) + '/public/' + file)
