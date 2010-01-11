de7deefe5fee2830b1e49b56e8f5d85bc86537c3
ciaranj <ciaranj@gmail.com>
2010-01-11 22:10:41 +0000

    Initial (non-ajax) V. Dull Flickr integration example with express

--- examples/flickr/public/javascripts/app.js
+++ examples/flickr/public/javascripts/app.js
@@ -1,35 +1,31 @@
+tb_pathToImage = '/public/images/loadingAnimation.gif';
+
+var old_tb_show= tb_show;
+var old_tb_remove= tb_remove;
+var pf,upf;
+
+tb_show=  function(caption, url, imageGroup){
+    old_tb_show(caption, url, imageGroup);
+    if(pf) pf();
+}
+
+tb_remove=  function() {
+    old_tb_remove();
+    if(upf) upf();
+}
+
 $(function(){
     var api_key= $('input[name=api_key]');
     var user_name= $('input[name=user_name]');
-/*    $("#scroller").jCarouselLite({
+     $("#scroller").jCarouselLite({
         auto: 800,
-        speed: 1000
+        speed: 1000,
+        visible: 4.5,
+        easing: "easeinout",
+        pauseOnHover: false,
+        referenceCallback: function(pauseFunction, unPauseFunction) {
+            pf= pauseFunction;
+            upf= unPauseFunction;
+        }
        });
-  */      
-    /*
-  // Send message
-  $('form').submit(function(){
-    var message = $('input[name=message]'),
-        name = $('input[name=name]')
-    if (message.val()) 
-      $.post('/chat', { message: $.trim(name.val()) + ': ' + message.val() }, function(){
-        message.val('')
-      })
-    else
-      message.css('border', '1px solid red')
-    return false
-  })
-  
-  // Longpoll
-  ;(function poll(){
-    $.getJSON('/chat/messages', function(messages){
-      $('#messages').empty()
-      $.each(messages, function(i, msg){
-        $('#messages')
-          .append('<li>' + msg + '</li>')
-          .get(0).scrollTop = $('#messages').get(0).scrollHeight
-      })
-      poll()
-    })
-  })() */
 })
