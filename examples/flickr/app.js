005cfc06764ec0cf14d0a709d3d6bdd442658714
ciaranj <ciaranj@gmail.com>
2010-01-18 23:17:50 +0000

    Updated to latest flickrnode and tweaked style to be similar to others ;)

--- examples/flickr/app.js
+++ examples/flickr/app.js
@@ -1,4 +1,9 @@
 var sys= require('sys');
+var FlickrAPI= require('./lib/flickr/lib/flickr').FlickrAPI;
+
+var INITIAL_API_KEY= "9a0554259914a86fb9e7eb014e4e5d52";
+var INITIAL_USER_NAME= "_rebekka";
+
 require.paths.unshift('lib')
 require('express')
 require('express/plugins')
@@ -10,54 +15,47 @@ configure(function(){
   set('root', dirname(__filename))
   enable('cache views')
   enable('cache statics')
+  enable('show exceptions')
 })
 
-var FlickrAPI= require('support/flickr/lib/flickr').FlickrAPI;
-
-function do_index(arguments) {
-    this.render('index.haml.html', {
-        locals: arguments
-      });
-}
-
-function fail(err) {
-    sys.puts(err)
-    
-    throw new Error(err.code + " -  " + err.message);
-};
-    
-get('/', function(){
-     do_index.apply(this,[{
-        user_name : '',
-        api_key : '',
-        photos : []
-      }])
-})
-
-post('/', function() {
-    var user_name= this.param('user_name');
-    var api_key= this.param('api_key');
+function do_index(user_name, api_key) {
     var flickr= new FlickrAPI(api_key);
     var request= this;
     flickr.people.findByUsername(user_name).addErrback(fail).addCallback(function(user){
         flickr.photosets.getList(user.id).addErrback(fail).addCallback(function(photosets){
             flickr.photosets.getPhotos(photosets.photoset[0].id, ["url_sq", "url_m"]).addErrback(fail).addCallback(function(photoset){
-                do_index.apply(request, [{
-                  user_name : user_name == null ? '' : user_name ,
-                  api_key : api_key == null ? '' : api_key,
-                  photos : photoset.photo === undefined ? [] : photoset.photo
-                }])
+                request.render('index.haml.html', {
+                    locals: {
+                      user_name : user_name == null ? '' : user_name ,
+                      api_key : api_key == null ? '' : api_key,
+                      photos : photoset.photo === undefined ? [] : photoset.photo
+                    }
+                  });
             });
         });
     });    
+}
+
+function fail(err) {
+    // Why does this pull the app down, and not get rendered as per the /error route below ?
+    throw new Error(err.code + " -  " + err.message);
+};
+    
+get('/', function(){
+     do_index.apply(this,[INITIAL_USER_NAME,INITIAL_API_KEY]);
+})
+
+post('/', function() {
+    do_index.apply(this,[this.param('user_name'),
+                         this.param('api_key') ]);
 })
 
 get('/public/*', function(file){
   this.sendfile(dirname(__filename) + '/public/' + file)
 })
 
-get('/error/view', function(){
-  this.render('does.not.exist')
+get('/*.css', function(file){
+  this.render(file + '.sass.css', { layout: false })
 })
 
 get('/error', function(){
